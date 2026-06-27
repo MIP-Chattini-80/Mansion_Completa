fb4a126c4c5e764aaa4694f6edcce49e2fb15244
MIP-Chattini-80 <djandr3s18@gmail.com>
2026-06-27 18:24:29 -0400

    Refactor: Implementación final de HATEOAS

--- personajes/src/main/java/com/mansion_2/personajes/Assemblers/PersonajesModelAssembler.java
+++ personajes/src/main/java/com/mansion_2/personajes/Assemblers/PersonajesModelAssembler.java
@@ -1,5 +1,24 @@
 package com.mansion_2.personajes.Assemblers;
 
-public class PersonajesModelAssembler {
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import com.mansion_2.personajes.controller.V2.PersonajesController;
+import com.mansion_2.personajes.dto.PersonajesDTO;
+
+@Component
+public class PersonajesModelAssembler
+        implements RepresentationModelAssembler<PersonajesDTO, EntityModel<PersonajesDTO>> {
+
+    @Override
+    public EntityModel<PersonajesDTO> toModel(PersonajesDTO categoria) {
+        return EntityModel.of(categoria,
+                linkTo(methodOn(PersonajesController.class).obtenerPorId(categoria.getId())).withSelfRel(),
+                linkTo(methodOn(PersonajesController.class).listarTodo()).withRel("categorias"));
+    }
 
 }
